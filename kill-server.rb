0cbfd49e8d96d1de0fadf25ca27bad9d55f1eb11
mj-963 <mjportfoliomail@gmail.com>
2024-07-11 14:09:13 +0100

    formula update

--- kill-server.rb
+++ kill-server.rb
@@ -2,7 +2,7 @@ class KillServer < Formula
     desc "A script to kill a server running on a specified port"
     homepage "https://github.com/mj-963/kill-server"
     url "https://github.com/mj-963/kill-server/archive/v1.0.0.tar.gz"
-    sha256 "0c6a8c45f2177fa24150e2d1a4e48497c745491f11c1e743c6f45cc56a1ff94b"
+    sha256 "mysha"
   
     def install
       bin.install "kill-server.sh" => "kill-server"
